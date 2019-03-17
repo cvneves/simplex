e8cb8bf8d6c33774b7a88c13f13014a65d699d97
carlos <carloskic1@gmail.com>
2019-03-17 16:18:41 -0300

    m

--- Simplex.h
+++ Simplex.h
@@ -10,21 +10,7 @@
 
 class Simplex
 {
-  public:
-    int n_variables, n_restrictions;
-    std::vector<double> c, b;
-    std::vector<double> current_solution;
-    bool is_optimal;
-    std::vector<std::vector<double>> A, B, N;
 
-    int FindPivotColumn();
-    int FindPivotRow();
-    void Pivoting(std::vector<double>& row, int pivot_row, int pivot_column);
-    void ObjFunctionPivoting();
-
-    void Solve();
-    
-    std::string ToString();
 
 };
 
--- main.cc
+++ main.cc
@@ -4,24 +4,5 @@
 
 int main()
 {
-    Simplex a;
-    a.n_restrictions = 2;
-    a.n_variables = 5;
-    a.c = {-2, -3, -4, 0, 0};
-    a.A = {{3, 2, 1, 1, 0}, {2, 5, 3, 0, 1}};
-    a.b = {10, 15};
 
-    a.Solve();
-
-    /*int i = a.FindPivotRow();
-    int j = a.FindPivotColumn();
-    a.Pivoting(a.A[0], i, j);
-    a.Pivoting(a.c, i, j); */
-
-    std::cout << a.ToString();
-
-    for (int ci = 0; ci < a.c.size(); ci++)
-    {
-        std::cout << a.c[ci] << " ";
-    }
 }
