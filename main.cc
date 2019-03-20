e302f439a29a8b482cfff5735aff14c828fc0d00
Carlos Vinicius <carloskic1@gmail.com>
2019-03-20 11:33:39 -0300

    AA

--- main.cc
+++ main.cc
@@ -4,9 +4,12 @@
 
 int main()
 {
+    /*https://jaredantrobus.com/squirrel/2015/Summer/MA162/4.1.php*/
+
+
     Simplex a;
     //a.tableau = {{1, 2, 3, 4, 0, 0, 0}, {0, 3, 2, 1, 1, 0, 10}, {0, 2, 5, 3, 0, 1, 15}};
-    a.tableau = {{1,-3,-2,0,0,0,0}, {0,-1,2,1,0,0,4}, {0,3,2,0,1,0,14}, {0,1,-1,0,0,1,3}};
+    a.tableau = {{1,-3,-2,0,0,0}, {0,2,3,1,0,12}, {0,2,1,0,1,8}};
 
    // std::cout << a.FindPivotColumn() << "\n";
    // std::cout << a.FindPivotRow(1) << "\n";
