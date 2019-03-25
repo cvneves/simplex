ce4e7980317c05348a48631b77fa4d9e5028c4cb
carlos <carloskic1@gmail.com>
2019-03-25 03:08:20 -0300

    a

--- parser/mps_parser.py
+++ parser/mps_parser.py
@@ -1,6 +1,6 @@
 f = open("p1.mps", "r")
 
-
+M = 1000000
 restrictions = {}
 variables = {}
 rhs = {}
@@ -17,15 +17,17 @@ for line in f:
 
     if section == 'ROWS':
         restrictions[l[1]] = l[0]
+        if l[0] == 'N':
+            obj_func_name = l[1]
 
     if section == 'COLUMNS':
         var_name = l[0]
         del l[0]
         if var_name not in variables:
-            variables[var_name] = {}
+            variables[var_name] = ({}, {})
         for c in range(0, len(l)):
             if c % 2 == 0:
-                variables[var_name][l[c]] = float(l[c+1])
+                variables[var_name][0][l[c]] = float(l[c+1])
 
     if section == 'RHS':
         rhs_name = l[0]
@@ -37,19 +39,55 @@ for line in f:
                 rhs[rhs_name][l[c]] = float(l[c+1])
 
     if section == 'BOUNDS':
-        if l[0] == 'UP':
-            bound_type = 'L'
-        elif l[0] == 'LO':
-            bound_type = 'G'
-        row_name = l[2] + "_" + l[0]
-        restrictions[row_name] = bound_type
-
-        variables[l[2]][row_name] = float(l[3])
+        variables[l[2]][1][l[0]] = float(l[3])
 
 
-# print(variables)
-# print(restrictions)
-# print(rhs)
+# standard form pass
+
+std_variables = variables
+std_restrictions = restrictions
+std_rhs = rhs
+
+for row in std_restrictions.items():
+    if row[1] == 'N':
+        continue
+    elif row[1] == 'L':
+        variables["SLACK_" + row[0]] = ({row[0]: 1.0}, {})
+    elif row[1] == 'G':
+        variables["SURPLUS_" + row[0]] = ({row[0]: -1.0}, {})
+        variables["ARTIF_" + row[0]] = ({row[0]: 1, obj_func_name: M}, {})
+    elif row[1] == 'E':
+        variables["ARTIF_" + row[0]] = ({row[0]: 1, obj_func_name: M}, {})
+
+for var in std_variables.items():
+    if var[1][1] == {}:
+        continue
+    elif 'FR' in var[1][1]:
+        std_variables[var[0]+"+"] = (std_variables[var[0]][0].copy(), std_variables[var[0]][1].copy()) 
+        std_variables[var[0]+"+"][1].clear()
+        std_variables[var[0]+"-"] = (std_variables[var[0]][0].copy(), std_variables[var[0]][1].copy()) 
+        std_variables[var[0]+"-"][1].clear()
+        
+        for i in std_variables[var[0]+"-"][0].items():
+            std_variables[var[0]+"-"][0][i[0]] = -i[1]         
+            del i
+
+        del std_variables[var[0]]
+    
+'''
+UP LO FX FR
+1  0  0  0
+0  0  1  0
+0  1  0  0
+1  1  0  0
+
+'''
+
+
+
+for v in std_variables.items():
+    print(v)
+#print(rhs)
 
 # tableau creation
 
@@ -89,9 +127,7 @@ for row in restrictions.items():
     pass
 
 
-for row in tableau:
-    print(row)
+# for row in tableau:
+#    print(row)
 
-print(n_artificial_vars)
-print(n_slack_vars)
 f.close()
