bb9a26aa3bdaa5b83d8d7a557368e214ef1ef4c1
Carlos Vinicius <carloskic1@gmail.com>
2019-03-26 19:16:55 -0300

    aa

--- main.cc
+++ main.cc
@@ -7,7 +7,7 @@ int main()
     /*https://jaredantrobus.com/squirrel/2015/Summer/MA162/4.1.php*/
     /*http://simplex.tode.cz/en/#steps*/
 
-    Simplex a;
+   /* Simplex a;
     double M = 10000;
     //a.tableau = {{1, 2, 3, 4, 0, 0, 0}, {0, 3, 2, 1, 1, 0, 10}, {0, 2, 5, 3, 0, 1, 15}};
     a.tableau = {{1,-3,-5,0,0,M,M,0}, {0,1,1,-1,0,1,0,2}, {0,0,1,0,1,0,0,6}, {0,3,2,0,0,0,1,18}};
@@ -15,5 +15,8 @@ int main()
 std::cout << a.ToString();
     a.Solve();
     std::cout << a.objective_value << "\n";
-    std::cout << a.ToString();
+    std::cout << a.ToString(); */
+
+    
+
 }
--- parser/mps_to_bin.py
+++ parser/mps_to_bin.py
@@ -2,10 +2,12 @@ import sys
 import mps_parser
 import struct
 
+
+
 file_name = sys.argv[1]
 
 tableau = mps_parser.MpsParse(file_name)
 
 for row in tableau:
     print(row)
-print((5).to_bytes(2,byteorder='big'))
+
