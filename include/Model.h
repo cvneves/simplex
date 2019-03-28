41a5aff98f566fce5ee0c52b9af71995a53e6f61
Carlos Vinicius <carloskic1@gmail.com>
2019-03-28 19:07:54 -0300

    a

--- include/Model.h
+++ include/Model.h
@@ -1,51 +1,70 @@
-#ifndef PROBLEM_H
-#define PROBLEM_H
+#ifndef MODEL_H
+#define MODEL_H
 
 #include <map>
 #include <string>
+#include <vector>
+
+enum ConstraintType
+{
+    less_equal,
+    equal,
+    greater_equal
+};
+
+enum ObjectiveType
+{
+    maximize,
+    minimize
+};
 
 class Constraint
 {
-
   public:
+    std::string name;
     std::map<std::string, double> column_value;
     std::map<std::string, double> rhs_value;
 
-    enum ConstraintType
-    {
-        less_equal,
-        equal,
-        greater_equal
-    };
-
+    ConstraintType constraint_type;
 };
 
-class Bound : public Constraint
+class Bound
 {
   public:
+    double value;
+    enum BoundType
+    {
+        less_equal,
+        greater_equal,
+        equal
+    };
+    BoundType bound_type;
 };
 
 class Variable
 {
   public:
     std::string name;
-    std::map<std::string, Bound> bound_values;
+    std::map<std::string, std::vector<Bound>> bounds;
 };
 
 class Objective
 {
   public:
+    std::string name;
     std::map<std::string, double> cost_value;
-    enum ObjectiveType
-    {
-        maximize,
-        minimize
-    };
+
+    ObjectiveType objective_type;
 };
 
 class Model
 {
   public:
+    Objective objective_function;
+    std::vector<Constraint> constraints;
+    std::vector<Variable> variables;
+
+    std::string ToString();
 };
 
 #endif
--- src/Model.cc
+++ src/Model.cc
@@ -3,10 +3,15 @@
 
 #include "Model.h"
 
+std::string Model::ToString()
+{
+    std::string obj_func;
+    obj_func = objective_function.objective_type == maximize ? "Max " : "Min ";
 
-
-
-
-
+    for(auto it = objective_function.cost_value.begin(); it != objective_function.cost_value.end(); ++it)
+    {
+        obj_func += std::to_string(*it);
+    }
+}
 
 #endif
