61207e10ef8a9fed8ee40a93dd7b08075e87d551
Carlos Vinicius <carloskic1@gmail.com>
2019-04-03 20:40:40 -0300

    a

--- src/Parser.cc
+++ src/Parser.cc
@@ -30,7 +30,7 @@ std::vector<std::string> StringSplit(std::string str)
                 i++;
                 break;
             }
-            else if(i == str.size() - 1)
+            else if (i == str.size() - 1)
             {
                 str_vec.push_back(std::string(str.begin(), str.begin() + i + 1));
                 i++;
@@ -40,7 +40,7 @@ std::vector<std::string> StringSplit(std::string str)
         }
         str = std::string(str.begin() + i, str.end());
         str = StringRemoveBorders(str);
-        }
+    }
 
     return str_vec;
 }
@@ -53,26 +53,105 @@ Model ReadMps(std::string file_name)
 
     std::string line;
 
+    // ROWS
+
     int line_counter = 0;
+
+    std::map<std::string, Constraint::ConstraintType> limits;
+    std::pair<std::string, Objective::ObjectiveType> obj_func;
+
     while (std::getline(f, line))
     {
-        /*if (line_counter == 0 || line_counter == 1)
+        if (line_counter == 0 || line_counter == 1)
         {
             line_counter++;
             continue;
         }
 
         if (line == "COLUMNS")
-            break; */
-        
+            break;
 
-        for(auto str : StringSplit(line))
+        auto str_vec = StringSplit(line);
+
+        if (str_vec[0] == "N")
         {
-            std::cout << "[" << str << "] ";
+            obj_func.first = str_vec[1];
+            obj_func.second = Objective::maximize;
         }
-        std::cout << "\n";
+        else if (str_vec[0] == "L")
+        {
+            limits[str_vec[1]] = Constraint::less_equal;
+        }
+        else if (str_vec[0] == "E")
+        {
+            limits[str_vec[1]] = Constraint::equal;
+        }
+        else if (str_vec[0] == "G")
+        {
+            limits[str_vec[1]] = Constraint::greater_equal;
+        }
 
         line_counter++;
+    }
+
+    // COLUMNS
+
+    line_counter = 0;
+
+    std::map<std::string, std::map<std::string, double>> variables;
+
+    while (std::getline(f, line))
+    {
+        if (line == "RHS")
+            break;
+
+        auto str_vec = StringSplit(line);
+
+        for (int i = 1; i < str_vec.size() - 1; i += 2)
+        {
+            variables[str_vec[0]][str_vec[i]] = std::stod(str_vec[i + 1]);
+        }
+    }
+
+    // RHS
+
+    std::map<std::string, std::map<std::string, double>> rhs;
+
+    while (std::getline(f, line))
+    {
+        if (line == "BOUNDS")
+            break;
+
+        auto str_vec = StringSplit(line);
+
+        for (int i = 1; i < str_vec.size() - 1; i += 2)
+        {
+            rhs[str_vec[0]][str_vec[i]] = std::stod(str_vec[i + 1]);
+        }
+
+  
+    }
+
+    // BOUNDS
+
+    std::map <std::string, std::pair<double, double>> bounds;
+
+    for(auto v : variables)
+    {
+        bounds[v.first] = ;
+    }
+
+    while (std::getline(f, line))
+    {
+        if (line == "ENDATA")
+            break;
+
+        auto str_vec = StringSplit(line);
+
+
+
+
+        std::cout << line << "\n";
     }
 
     return m;
