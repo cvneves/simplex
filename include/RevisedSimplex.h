3d7a5ad0528b60d6b131145615f199f37370c7bb
Carlos Vinicius <carloskic1@gmail.com>
2019-04-11 15:01:58 -0300

    a

--- include/RevisedSimplex.h
+++ include/RevisedSimplex.h
@@ -3,18 +3,18 @@
 
 #include "Eigen/Dense"
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 class RevisedSimplex
 {
-  public:
-    Eigen::MatrixXd A, B;
-    Eigen::VectorXd x, d, c, b;
-    
-    std::vector<int> basic_variables, non_basic_variables;
+public:
+  Eigen::MatrixXd A, B;
+  Eigen::VectorXd x, d, c, b;
 
-    int FindSmallestReducedCost();
+  std::vector<int> basic_variables, non_basic_variables;
 
-
+  int FindSmallestReducedCost();
 };
 
 #endif
--- src/RevisedSimplex.cc
+++ src/RevisedSimplex.cc
@@ -1,36 +1,45 @@
 #include "RevisedSimplex.h"
-#include <iostream>
 
 int RevisedSimplex::FindSmallestReducedCost()
 {
-    Eigen::VectorXd c_B(B.size()), reduced_costs(c.size());
+    Eigen::VectorXd c_B(B.col(0).size()), reduced_costs(c.size());
+    Eigen::MatrixXd p(1, B.col(0).size());
+
+    std::sort(basic_variables.begin(), basic_variables.end());
+    std::sort(non_basic_variables.begin(), non_basic_variables.end());
+
 
     for (int i = 0; i < c_B.size(); i++)
     {
         c_B[i] = c[basic_variables[i]];
     }
+    
+    p = c_B.transpose() * B.inverse();
+
 
     for (int j = 0; j < c.size(); j++)
     {
         for (auto &i : non_basic_variables)
         {
-            if(j == i)
+            if (j == i)
                 d[i] = 1;
-            else 
-                d[i] = 0;            
+            else
+                d[i] = 0;
         }
 
         Eigen::VectorXd d_B(B.size());
 
         d_B = (-1) * B.inverse() * A.col(j);
 
-        for(int i = 0; i < basic_variables.size(); i++)
+        for (int i = 0; i < basic_variables.size(); i++)
         {
             d[basic_variables[i]] = d_B[i];
         }
+  
+        //reduced_costs[j] = c[j] - (p * A.col(j))[0];
+        reduced_costs[j] = c[j] + (c_B.transpose() * d_B)[0];
 
-       std::cout << c_B.transpose() << "\n";
+
     }
-
-
+    std::cout << reduced_costs;
 }
